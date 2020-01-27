c4becde05ddb2b8936dabca876b2da96762d62d2
Amanda <amandacarvalhopintodeazevedo@gmail.com>
2020-01-27 13:56:03 -0300

    Continuação Aula ASP.NET

---
+++ TestandoASPNet/TestandoASPNet/Context/Aula1Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TestandoASPNet.Models;
+
+namespace TestandoASPNet.Context
+{
+    public class Aula1Context : DbContext
+    {
+        public Aula1Context()
+            : base("Aula1Context")
+        {
+
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+        
+    }
+}
---
+++ TestandoASPNet/TestandoASPNet/Models/Cliente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace TestandoASPNet.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(150, ErrorMessage = "Máximo de caracteres excedido")]
+        [MinLength(2, ErrorMessage = "Mínimo de 2 caracteres")]
+        [DisplayName("Nome do Cliente")]
+        [Required(ErrorMessage = "Preencher campo Nome")]
+        public string Nome { get; set; }
+
+
+        [MaxLength(150, ErrorMessage = "Máximo de caracteres excedido")]
+        [MinLength(2, ErrorMessage = "Mínimo de 2 caracteres")]
+        [DisplayName("Sobrenome do Cliente")]
+        [Required(ErrorMessage = "Preencher campo Sobrenome")]
+        public string SobreNome { get; set; }
+
+
+        [ScaffoldColumn(false)]
+        public DateTime DataCadastro { get; set; }
+
+
+        [MaxLength(150, ErrorMessage = "Máximo de caracteres excedido")]
+        [MinLength(2, ErrorMessage = "Mínimo de 2 caracteres")]
+        [DisplayName("E-mail")]
+        [Required(ErrorMessage = "Preencher campo e-mail")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+    }
+}
